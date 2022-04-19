0e027a6b02819e49d962e95c05e40b391ac5c6c5
Marin37 <57327931+Marin37@users.noreply.github.com>
2022-04-19 00:00:18 -0300

    Initial Commit
    Subir a GH, faltan funciones de insertar y borrar

---
+++ Programa/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Cancelar = New System.Windows.Forms.Button()
+        Me.Terminar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(47, 65)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(180, 111)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Cargar Datos"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(275, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(228, 173)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(350, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Resultado:"
+        '
+        'Cancelar
+        '
+        Me.Cancelar.Location = New System.Drawing.Point(47, 196)
+        Me.Cancelar.Name = "Cancelar"
+        Me.Cancelar.Size = New System.Drawing.Size(75, 42)
+        Me.Cancelar.TabIndex = 3
+        Me.Cancelar.Text = "Cancelar"
+        Me.Cancelar.UseVisualStyleBackColor = True
+        '
+        'Terminar
+        '
+        Me.Terminar.Location = New System.Drawing.Point(152, 196)
+        Me.Terminar.Name = "Terminar"
+        Me.Terminar.Size = New System.Drawing.Size(75, 42)
+        Me.Terminar.TabIndex = 4
+        Me.Terminar.Text = "Terminar"
+        Me.Terminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 277)
+        Me.Controls.Add(Me.Terminar)
+        Me.Controls.Add(Me.Cancelar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Cancelar As System.Windows.Forms.Button
+    Friend WithEvents Terminar As System.Windows.Forms.Button
+
+End Class
---
+++ Programa/Form1.vb
@@ -0,0 +1,164 @@
+﻿Public Class Form1
+    Dim Nombre As String
+    Dim Tiempo As String
+    Dim Tecla As String
+    Dim VCierre As String
+    Dim GuardCierre As Integer
+    Dim Ruta As String
+    Dim Terminado As Boolean = False
+    Dim File As System.IO.StreamWriter
+
+    
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        MsgBox("Bienvenido, antes de crear el script hay que definir ciertos parámetros, puede cancelar al final del formulario", vbOKOnly, "Introducción")
+
+        ' Tiempo entre inputs en milisegundos (ms)
+        Tiempo = ""
+        While IsNumeric(Tiempo) = False
+            Tiempo = InputBox("Tiempo en milisegundos (ms) entre cada dato y Enter. 200 mínimo recomendado, puede poner un valor más alto para verlo cargar todo correctamente", "Definir tiempo entre datos", "200")
+        End While
+
+        ' Tecla a utilizar
+        Tecla = "F7"
+        Tecla = InputBox("Tecla para activar el script, se recomienda alguna que no se use con frecuencia, por defecto F7. Se recomiendan las teclas de Función y utilizar mayúscula", "Definir tecla", "F7")
+
+        ' Valor para cerrar
+        VCierre = 0
+        VCierre = InputBox("Valor que se utilizara de cierre, puede ser 0, XX, etc. Para terminar de cargar datos tendrá que introducir dicho valor", "Definir valor cierre", "0")
+
+        ' Elije si quiere guardar al final o no el valor de cierre
+        GuardCierre = MsgBox("¿Crear el script SIN cargar el valor de cierre (" & VCierre & ") al final? (para corte de control por ejemplo)", MsgBoxStyle.YesNo)
+        If GuardCierre = vbYes Then
+            GuardCierre = 0
+            MsgBox("El valor NO se cargará a lo último", MsgBoxStyle.OkOnly)
+        ElseIf GuardCierre = vbNo Then
+            GuardCierre = 1
+            MsgBox("El último valor será " & VCierre, MsgBoxStyle.OkOnly)
+        End If
+
+        ' Nombre del archivo
+        Nombre = InputBox("Nombre o sufijo del archivo (el output será <tecla> - <nombre>)", "Definir Nombre")
+        Nombre = Tecla & " - " & Nombre & ".ahk"
+
+        ' Opcion guardar dentro del zip o .exe standalone
+
+        MsgBox("Si está usando el programa desde el zip, se recomienda que guarde en la carpeta que viene incluida. Si está utilizando esta aplicación standalone desde el ejecutable (sin el proyecto) entonces se utilizará una carpeta en el mismo directorio (si no se creó antes). Puede también elegir no usar carpeta", MsgBoxStyle.Information)
+        If (MsgBox("¿Está utilizando el programa desde el .zip?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes) Then
+            ' Ruta si está en zip
+            Ruta = "..\..\..\Scripts Resultado\"
+        ElseIf (MsgBox("¿Quiere crear la carpeta <Scripts Resultado>?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes) Then
+            ' Ruta directa (concarpeta)
+            Ruta = ".\Scripts Resultado\"
+        Else
+            ' Ruta directa (sin carpeta)
+            Ruta = ".\"
+        End If
+
+        ' Borrar archivo viejo si existe o cambiar nombre
+        While System.IO.File.Exists(Ruta & Nombre)
+            If (MsgBox("Ya hay un archivo con el mismo nombre, ¿Desea eliminarlo?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes) Then
+                System.IO.File.Delete(Ruta & Nombre)
+                MsgBox("Archivo viejo borrado", MsgBoxStyle.Information)
+            Else
+                Nombre = InputBox("Ingrese un nombre distinto (el output será <tecla> - <nombre>)", "Renombrar")
+                Nombre = Tecla & " - " & Nombre & ".ahk"
+            End If
+        End While
+
+        ' Crear carpeta si no existe
+        If ((Ruta <> ".\") And (Not System.IO.Directory.Exists(Ruta))) Then
+            System.IO.Directory.CreateDirectory(Ruta)
+        End If
+
+
+
+        ' Abrir (instanciar) archivo
+        File = My.Computer.FileSystem.OpenTextFileWriter(Ruta & Nombre, True)
+
+        ' Escribir configuraciones
+        With File
+            .WriteLine("#SingleInstance Force ")
+            .WriteLine("#NoEnv ")
+            .WriteLine("SendMode Input ")
+            .WriteLine("SetWorkingDir %A_ScriptDir% ")
+            .WriteLine("Tiempo = " & Tiempo)
+            .WriteLine("")
+            .WriteLine("")
+            .WriteLine(Tecla & "::")
+        End With
+    End Sub
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Text As String
+
+        Text = "a"
+        While Text <> VCierre
+
+            Text = InputBox("Ingresar texto", "Text", VCierre)
+
+            ' Verificar que no quede vacío
+            If Text <> "" Then
+                If GuardCierre = 1 Then ' Ejecucion guardando último valor
+                    ListBox1.Items.Add(Text)
+                    With File
+                        .WriteLine("")
+                        .WriteLine("Send, " & Text)
+                        .WriteLine("Send {Enter}")
+                        .WriteLine("Sleep, %Tiempo%")
+                    End With
+
+                ElseIf GuardCierre = 0 Then ' Ejecucion sin guardar ultimo valor
+                    If Text <> VCierre Then ' Solo verifica extra
+                        ListBox1.Items.Add(Text)
+                        With File
+                            .WriteLine("")
+                            .WriteLine("Send, " & Text)
+                            .WriteLine("Send {Enter}")
+                            .WriteLine("Sleep, %Tiempo%")
+                        End With
+                    End If
+                End If
+            End If
+
+        End While
+
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Escape Then Me.Close()
+    End Sub
+
+    Private Sub Cancelar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancelar.Click
+        ' Operación cancelada
+        File.Close()
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_CerrarOCancelar(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        If Terminado = False Then
+            System.IO.File.Delete(Ruta & Nombre)
+            MsgBox("Script cancelado", MsgBoxStyle.Exclamation)
+        End If
+
+        ' Operación cancelada        
+        Me.Hide()
+
+    End Sub
+
+    Private Sub Terminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Terminar.Click
+
+        ' Operación exitosa
+        Terminado = True
+        File.Close()
+        MsgBox("Gracias, recuerde que para utilizar el script se requiere AutoHotKey", MsgBoxStyle.MsgBoxRight)
+        Me.Close()
+
+    End Sub
+
+    
+End Class
